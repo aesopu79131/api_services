dec3a2186551397e2dc4c3a79c5c1c59b268ee4b
dragon nip <dragonkcnip@sunevision.com>
2026-03-17 17:30:38 +0800

    init nextJS

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 sudo chown -R nginx:nginx /var/www/html
 sudo chmod -R 755 /var/www/html
+
+# 重新產生 SSL（如果過期）
+# certbot renew --quiet || true
+
 systemctl start nginx
 if ! systemctl is-active --quiet nginx; then
   echo "Nginx failed to start"
