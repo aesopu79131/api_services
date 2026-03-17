df4ae644894dbf4deea5fc467eb972e658cca90f
dragon nip <dragonkcnip@sunevision.com>
2026-03-17 14:19:36 +0800

    test

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-sudo chown -R www-data:www-data /var/www/html
+sudo chown -R nginx:nginx /var/www/html
 sudo chmod -R 755 /var/www/html
 systemctl start nginx
+if ! systemctl is-active --quiet nginx; then
+  echo "Nginx failed to start"
+  exit 1
+fi
